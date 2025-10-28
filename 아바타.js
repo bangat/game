275155a865e8e49d8566bf0143333f691fcd6e72
bangat <130852268+bangat@users.noreply.github.com>
2025-10-29 02:26:08 +0900

    Add files via upload

---
+++ 아바타.js
@@ -0,0 +1,26 @@
+// avatars.js
+
+const AVATAR_SETS = {
+    'default_bomber': {
+        front: './아바타폴더/기본캐정면.png',
+        back: './아바타폴더/기본캐뒷면.png',
+        left: './아바타폴더/기본캐좌측.png',
+        right: './아바타폴더/기본캐우측.png'
+    },
+    'penguin_parka': {
+        front: './아바타폴더/펭귄정면.png',
+        back: './아바타폴더/펭귄뒷면.png',
+        left: './아바타폴더/펭귄좌측.png',
+        right: './아바타폴더/펭귄우측.png'
+    },
+    'puppy_set': {
+        front: './아바타폴더/강아지정면.png',
+        back: './아바타폴더/강아지뒷면.png',
+        left: './아바타폴더/강아지좌측.png',
+        right: './아바타폴더/강아지우측.png'
+    }
+    // 여기에 새로운 아바타 세트를 추가하면 됩니다.
+};
+
+// 필요한 경우 다른 공용 데이터도 여기에 추가할 수 있습니다.
+// 예: const ITEM_ICONS = { ... };
